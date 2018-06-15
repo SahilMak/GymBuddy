6f4e825c5c0ebd45c8824d148ca352e59d18c79b
Sahil Makhijani <svmakhijani94@gmail.com>
2018-06-15 01:46:39 -0400

    Removed unused imports

--- app/src/main/java/com/sahilmak/me/gymbuddy/MainActivity.java
+++ app/src/main/java/com/sahilmak/me/gymbuddy/MainActivity.java
@@ -1,9 +1,6 @@
 package com.sahilmak.me.gymbuddy;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -72,15 +69,15 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_profile) {
-            // Handle the camera action
+            // Profile view
         } else if (id == R.id.nav_log_workout) {
             // Do nothing
         } else if (id == R.id.nav_new_workout) {
-
+            // New workout view
         } else if (id == R.id.nav_strength) {
-
+            // Strength graph view
         } else if (id == R.id.nav_cardio) {
-
+            // Cardio graph view
         }
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
