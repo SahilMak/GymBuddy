2fc48b89de2d60ded64f1956c702a9c5c616575b
Sahil Makhijani <svmakhijani94@gmail.com>
2018-06-15 23:16:05 -0400

    Created empty fragment for adding exercise

---
+++ app/src/main/java/com/sahilmak/me/gymbuddy/AddExercise.java
@@ -0,0 +1,28 @@
+package com.sahilmak.me.gymbuddy;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AddExercise extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.add_exercise, container, false);
+        return view;
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+}
--- app/src/main/java/com/sahilmak/me/gymbuddy/MainActivity.java
+++ app/src/main/java/com/sahilmak/me/gymbuddy/MainActivity.java
@@ -2,6 +2,8 @@ package com.sahilmak.me.gymbuddy;
 
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -66,6 +68,8 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
+        Fragment fragment = null;
+        Class fragmentClass = null;
         int id = item.getItemId();
 
         if (id == R.id.nav_profile) {
@@ -73,11 +77,22 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_log_workout) {
             // Do nothing
         } else if (id == R.id.nav_new_workout) {
-            // New workout view
+            fragmentClass = AddExercise.class;
         } else if (id == R.id.nav_strength) {
             // Strength graph view
         } else if (id == R.id.nav_cardio) {
             // Cardio graph view
+        }
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (fragment != null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.content_main, fragment).commit();
         }
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
