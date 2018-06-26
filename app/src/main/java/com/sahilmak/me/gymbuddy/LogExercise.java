038f29c091a4defc9d57d97676e9d52d1682a6eb
Sahil Makhijani <svmakhijani94@gmail.com>
2018-06-26 00:43:33 -0400

    Created new fragment for logging workout

---
+++ app/src/main/java/com/sahilmak/me/gymbuddy/LogExercise.java
@@ -0,0 +1,27 @@
+package com.sahilmak.me.gymbuddy;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class LogExercise extends Fragment {
+    TextView name;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_log_exercise, container, false);
+
+        name = view.findViewById(R.id.log_name);
+        Bundle args = getArguments();
+        name.setText(args.getString("name"));
+
+        // Inflate the layout for this fragment
+        return view;
+    }
+}
