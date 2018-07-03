e2cbfb552d1c9c50a0553409e82fb4a62595eb2a
Sahil Makhijani <svmakhijani94@gmail.com>
2018-07-03 12:07:15 -0400

    Log workout populates with exercise image and name

--- app/src/main/java/com/sahilmak/me/gymbuddy/LogWorkout.java
+++ app/src/main/java/com/sahilmak/me/gymbuddy/LogWorkout.java
@@ -1,25 +1,34 @@
 package com.sahilmak.me.gymbuddy;
 
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class LogWorkout extends Fragment {
-    TextView name;
+    ImageView logImage;
+    TextView logName;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.log_workout, container, false);
 
-        name = view.findViewById(R.id.log_name);
+        logImage = view.findViewById(R.id.logImage);
+        logName = view.findViewById(R.id.logName);
+        // Populate with exercise info
         Bundle args = getArguments();
-        name.setText(args.getString("name"));
+        byte[] byteImage = args.getByteArray("image");
+        Bitmap bitmapImage = BitmapFactory.decodeByteArray(byteImage, 0, byteImage.length);
+        logImage.setImageBitmap(bitmapImage);
+        logName.setText(args.getString("name"));
 
         // Inflate the layout for this fragment
         return view;
